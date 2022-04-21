77a92b2676f70d29092b3626a2b20adc1d065381
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-04-21 18:58:12 +0530

    21-04-2022

---
+++ api_connection.py
@@ -0,0 +1,26 @@
+
+# Not completed
+
+"""This module that pull data from the api and upload to in s3."""
+import requests
+import json
+import os
+
+
+# response = requests.get('https://www.metaweather.com/api/location/44418/2013/4/27/')
+# print(response.json())
+
+class PullDataFromApi:
+    def __init__(self) -> None:
+        """This is the init method of the class of PullDataFromApi"""
+        pass
+    
+    
+    
+def main():
+    """This is the main method for the module api_connection"""
+    pull_data = PullDataFromApi()
+
+if __name__ == '__main__':
+    main()
+        
